e9150b4635c62c30b556a14d81de9203604c4a4f
Asadullah Nizamani <156411879+Asadniz@users.noreply.github.com>
2024-07-20 18:38:39 +0500

    Update errorPage.js

--- Weather App/errorPage.js
+++ Weather App/errorPage.js
@@ -4,7 +4,7 @@ let cityInput = document.querySelector('#cityInput');
 let submitForm = document.querySelector('#cityInputForm');
 submitForm.addEventListener('submit', function (event) {
     event.preventDefault();
-    axios.get(`https://api.weatherapi.com/v1/forecast.json?key=?&q=${cityInput.value}&days=5&aqi=no&alerts=no`)
+    axios.get(`https://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${cityInput.value}&days=5&aqi=no&alerts=no`)
         .then((response) => {
             localStorage.setItem('weatherData', JSON.stringify(response.data));
             window.location.href = 'weatherResult.html';
