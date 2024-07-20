edc43768e5dd32f5527f51652e218f2b3f153def
Asadullah Nizamani <156411879+Asadniz@users.noreply.github.com>
2024-07-20 18:39:01 +0500

    Update weatherApp.js

--- Weather App/weatherApp.js
+++ Weather App/weatherApp.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+const apiKey = process.env.API_KEY;
 let cityInput = document.querySelector('#cityInput');
 let submitForm = document.querySelector('#cityInputForm');
 let suggestions = document.querySelector('#suggestions');
@@ -9,7 +11,7 @@ title.addEventListener('click', function () {
 
 submitForm.addEventListener('submit', function (event) {
     event.preventDefault();
-    axios.get(`https://api.weatherapi.com/v1/forecast.json?key=?&q=${cityInput.value}&days=5&aqi=no&alerts=no`)
+    axios.get(`https://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${cityInput.value}&days=5&aqi=no&alerts=no`)
         .then((response) => {
             cityInput.value = ''
             localStorage.setItem('weatherData', JSON.stringify(response.data));
